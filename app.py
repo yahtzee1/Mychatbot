cbcbb467bea2a7b31de81f7e0f302eeb24401cea
Nishith Jain <KingNish@users.noreply.huggingface.co>
2024-05-24 16:33:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from huggingface_hub import InferenceClient
 
 
 def client_fn(model):
-    if "Mixtral" in model:
-        return InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
-    elif "Llama" in model:
-        return InferenceClient("meta-llama/Meta-Llama-3-8B-Instruct")
+    if "Nous" in model:
+        return InferenceClient("NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO")
+    elif "Star" in model:
+        return InferenceClient("HuggingFaceH4/starchat2-15b-v0.1")
     elif "Mistral" in model:
         return InferenceClient("mistralai/Mistral-7B-Instruct-v0.3")
     elif "Phi" in model:
         return InferenceClient("microsoft/Phi-3-mini-4k-instruct")
     else: 
-        return InferenceClient("microsoft/Phi-3-mini-4k-instruct")
+        return InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
 
 system_instructions1 = "[SYSTEM] Your task is to Answer the question. Keep conversation very short, clear and concise. The expectation is that you will avoid introductions and start answering the query directly, Only answer the question asked by user, Do not say unnecessary things.[QUESTION]"
 
@@ -40,5 +40,5 @@ def models(text, model="Mixtral 8x7B"):
 description="""# Chat GO
 ### Inspired from Google Go"""
 
-demo = gr.Interface(description=description,fn=models, inputs=["text", gr.Dropdown([ 'Mixtral 8x7B','Llama 3 8B','Mistral 7B v0.3','Phi 3 mini', ], value="Mistral 7B v0.3", label="Select Model") ], outputs="text", live=True, batch=True, max_batch_size=1000)
+demo = gr.Interface(description=description,fn=models, inputs=["text", gr.Dropdown([ 'Mixtral 8x7B','Nous Hermes Mixtral 8x7B DPO','StarChat2 15b','Mistral 7B v0.3','Phi 3 mini', ], value="Mistral 7B v0.3", label="Select Model") ], outputs="text", live=True, batch=True, max_batch_size=1000)
 demo.launch()
