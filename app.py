6a8eb37928b5db68888e9f5bb5d6e157b6df10a6
Nishith Jain <KingNish@users.noreply.huggingface.co>
2024-05-28 05:59:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def models(text, model="Mixtral 8x7B"):
     for response in stream:
         if "Phi" in model:
             if output.endswith("<|assistant|>"):
-                output = output(-13)
+                output = output[:-13]
         else:      
             if not response.token.text == "</s>":
                 output += response.token.text
