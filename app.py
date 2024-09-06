85d20c755fb59bf32f3fce01bf8e739ce38006d9
Ahmad Seayar Sroosh <Seayar@users.noreply.huggingface.co>
2024-09-06 20:39:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ client = InferenceClient("google/gemma-1.1-7b-it")
 def answer_question(role,question):
 
     params = {
-        "maxtoken":150,
+        "max_tokens":150,
         "temperature": 0.7
     }
 
